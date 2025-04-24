3f40302b583d4ed128fde09a87df6f0b0a06c7b8
Joel May <git@jmay.us>
2025-04-23 19:23:10 -0700

    Add support for aarch64
    wsocat is from https://github.com/vi/websocat/releases/tag/v1.13.0 websocat.aarch64-unknown-linux-musl
    
    sas2ircu, sas3ircu, sas2flash, sas3flash, storcli, and storcli2 are
    closed source and unavailable for aarch64. :(

--- update_chelsio.py
+++ update_chelsio.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import platform
 import subprocess
 
 
@@ -9,28 +10,39 @@ import subprocess
 BASE = "tools/chelsio_adapter_config_v4"
 
 
+def update(src_directory, arch):
+    dest_directory_common = "files/common"
+    dest_directory_arch = f"files/{arch}"
+    bin_suffix = "_arm" if arch == "arm64" else ""
+
+    os.makedirs(f"{dest_directory_arch}/usr/sbin", exist_ok=True)
+    os.makedirs(f"{dest_directory_common}/usr/local/libexec", exist_ok=True)
+    os.makedirs(f"{dest_directory_common}/usr/lib/firmware/cxgb4/config", exist_ok=True)
+
+    subprocess.check_call(
+        f"cp {src_directory}/{BASE}/bin/new/chelsio_adapter_config.py {dest_directory_common}/usr/local/libexec",
+        shell=True,
+    )
+    subprocess.check_call(
+        f"cp {src_directory}/{BASE}/vpds/* {dest_directory_common}/usr/lib/firmware/cxgb4/config/",
+        shell=True,
+    )
+    subprocess.check_call(
+        f"mv {src_directory}/{BASE}/bin/t5seeprom_bins/t5seeprom{bin_suffix} {dest_directory_arch}/usr/sbin/t5seeprom",
+        shell=True,
+    )
+    subprocess.check_call(
+        f"mv {src_directory}/{BASE}/bin/t6seeprom_bins/t6seeprom{bin_suffix} {dest_directory_arch}/usr/sbin/t6seeprom",
+        shell=True,
+    )
+
 if __name__ == "__main__":
     source = glob.glob("ChelsioUwire-*.tar.gz")[0]
-    directory = source.removesuffix(".tar.gz")
+    src_directory = source.removesuffix(".tar.gz")
+
     subprocess.check_call(["tar", "xf", source])
     try:
-        os.makedirs("files/usr/sbin", exist_ok=True)
-        os.makedirs("files/usr/lib/firmware/cxgb4/config", exist_ok=True)
-        subprocess.check_call(
-            f"mv {directory}/{BASE}/bin/new/chelsio_adapter_config.py files/usr/local/libexec",
-            shell=True,
-        )
-        subprocess.check_call(
-            f"mv {directory}/{BASE}/vpds/* files/usr/lib/firmware/cxgb4/config/",
-            shell=True,
-        )
-        subprocess.check_call(
-            f"mv {directory}/{BASE}/bin/t5seeprom_bins/t5seeprom files/usr/sbin/",
-            shell=True,
-        )
-        subprocess.check_call(
-            f"mv {directory}/{BASE}/bin/t6seeprom_bins/t6seeprom files/usr/sbin/",
-            shell=True,
-        )
+        update(src_directory, "amd64")
+        update(src_directory, "arm64")
     finally:
-        subprocess.check_call(["rm", "-rf", directory])
+        subprocess.check_call(["rm", "-rf", src_directory])
